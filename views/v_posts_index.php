598fe5e37dcf4e7c68ed8ea94fcc6cb81022b5c2
Zach Postle <zpostle@gmail.com>
2013-11-06 15:02:34 -0500

    Added user post time-stamp for post view.

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,6 +1,8 @@
+
 <?php foreach($posts as $post): ?>
 
-	<strong><?=$post['first_name']?></strong><br>
-	<?=$post['content']?><br><br>
-
+        <strong><?=$post['first_name']?> posted on <?=Time::display($post['created'])?></strong><br>
+        <?=$post['content']?><br><br>
+        
 <?php endforeach; ?>
+
