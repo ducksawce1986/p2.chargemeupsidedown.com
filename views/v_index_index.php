038d3aeadf37ceed4ba03df3f0823bcf72ae581d
Zach Postle <zpostle@gmail.com>
2013-11-06 14:09:52 -0500

    Added period to end of greeting.

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,5 +1,5 @@
 <?php if($user): ?>
-	Well hey there. We're glad you're back <?=$user->first_name;?>
+	Well hey there. We're glad you're back <?=$user->first_name;?>.
 <?php else: ?>
 	Why hello there! You're new here aren't you? Welcome to your newest
 	favorite site. This is Charge Me Upside Down!
