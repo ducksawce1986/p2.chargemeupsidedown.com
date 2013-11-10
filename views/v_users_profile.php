d0251b00538c9de086c7746cc206111268592a78
Zach Postle <zpostle@gmail.com>
2013-11-09 19:26:26 -0500

    Updated titles and associated documentation.

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,7 +1,48 @@
+<?php if($user): ?> 
+    <h1><?=$user->first_name?>'s Profile</h1><br>
+
+    <div id="infobox_avatar">
+<!-- Display user's avatar if uploaded -->
+    <img class="avatar" src="/uploads/avatars/<?= $user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>">
+<!-- Display Form For Uploading -->
+        <?php if ($user->image  == 'placeholder.jpg'): ?>
+            <div id="avatar_update_form">
+                <p>Would you like to upload your own photo?</p> 
+            <!-- Uploading Photo -->
+                <form role="form" method='POST' enctype="multipart/form-data" action='/users/profile_update/'>
+                    <div>
+                        <input type="file" id="avatar" name="avatar">
+                    </div>
+                    <button type="submit" class="btn btn-custom">Update Your Profile Image</button>
+                </form> 
+            </div>
+        <?php endif; ?>
+    </div>
 
 
-<?php if(isset($user_name)): ?>
-	<h1>This is the profile for <?=$user_name?></h1>
+<!-- User's Posts Appearing Below -->
+    <div id="infobox_posts">
+        <?php foreach($posts as $post): ?>
+            <article>
+            <!-- Time Display -->
+                <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+                    <?=Time::display($post['created'])?>
+                </time>
+            
+            <!-- Post Display -->
+                <div id="posts">
+                    <p><?=$post['content']?></p>            
+                </div>
+            <!-- Delete Option -->
+                <?php if($post['user_id'] == $user->user_id): ?>
+                    <div id="delete_option">          
+                        <a href='/posts/confirm_delete/<?=$post['post_id']?>'>Delete</a>
+                    </div>
+                <?php endif; ?>
+            </article>
+        <?php endforeach; ?>
+    </div>
+
 <?php else: ?>
-	<h1>No user has been specified</h1>
-<?php endif; ?>
+    <h1>No user specified</h1>
+<?php endif; ?> 
