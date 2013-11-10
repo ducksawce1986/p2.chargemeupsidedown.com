56f30d4a6f19b17f9bcd805fbe1199596121d871
Zach Postle <zpostle@gmail.com>
2013-11-09 19:53:05 -0500

    Made updates to view, and associated documentation.

---
+++ views/v_users_success.php
@@ -0,0 +1,7 @@
+<h1>Welcome to Charge Me Up!</h1>
+
+<p>It's time for you to speak up. Just click and enter the stage below, and you'll be taken to your login page. Enjoy!</p>
+
+<a href='/'>
+<img src="http://www.elamjung.com/image/from%20the%20stage%20%20Katmanu,%20Nepal.jpg" alt="Stage Left"></a>
+
