cbd85897708fbfea9452941e9b5f4b9afa3ed752
Zach Postle <zpostle@gmail.com>
2013-11-09 19:10:05 -0500

    Updated titles and associated documentation.

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -4,6 +4,12 @@
 
 	<br>
 
-	<input type='Submit' value='Add new post'>
+	<input type='Submit' value='Speak Your Mind'>
 
 </form>
+
+<div id="inner_infobox_left">
+	<img class="avatar" src="http://us.123rf.com/400wm/400/400/sergo/sergo1107/sergo110700333/9903129-vector-hand-drawn-studio-microphone-and-stars.jpg" alt = "Charge Up">
+	<h2>It's time for you to speak up.</h2>
+	<br>
+</div>
