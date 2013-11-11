d380eedc612df13022e47bffde73f57b83a326c1
Zach Postle <zpostle@gmail.com>
2013-11-11 02:32:47 -0500

    Updated default DB name for app.

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'zpostle@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_chargemeupsidedown_com'); 
+	define('DB_NAME', 'chargeme_p2_chargemeupsidedown_com'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
